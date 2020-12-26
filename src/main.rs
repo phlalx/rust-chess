f68ab1360cf668479fd0b4f21a8e87ac27a85028
phlalx <phlalx@users.noreply.github.com>
2020-12-26 11:37:07 +0100

    REPL: quit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use std::io;
 use std::io::Write;
 
+fn evaluate_command(command: &String) {
+    let mut tokens = command.split_ascii_whitespace();
+    let first = tokens.next().expect("No token found");
+    println!("first token is {}", first);
+    match first.as_ref() {
+        "quit" => {
+            println!("Bye!");
+            std::process::exit(0);
+        },
+        _ => (),
+    }
+}
+
 fn main() {
     let mut command = String::new();
     loop {
         print!("> ");
         io::stdout().flush().expect("Failed flushing stdout");
         io::stdin().read_line(&mut command).expect("Failed to read command");
-        println!("I got your command {}", command);
+        evaluate_command(&command);
     }
 }
