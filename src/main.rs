fdbc443a91a6e40a2ae99c0eba93bdba38dc8d56
phlalx <phlalx@users.noreply.github.com>
2020-12-26 11:06:43 +0100

    Simple read loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command = String::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed flushing stdout");
+        io::stdin().read_line(&mut command).expect("Failed to read command");
+        println!("I got your command {}", command);
+    }
 }
